4be550eb530f467e4cdd4d42016a354c07c0561e
Lapis <88260244+LapisPhoenix@users.noreply.github.com>
2022-08-18 20:25:54 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 ALERT = False  # Change to true if you want @everyone to be pinged
-directPing = "USER ID"   # Enter a username EX: 140089842421814951 for direct ping
+directPing = "USER ID"   # Enter a username EX: "140089842421814951" for direct ping
 webhook = ""  # Input your webhook
